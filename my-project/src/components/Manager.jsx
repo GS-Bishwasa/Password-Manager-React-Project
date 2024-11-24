b22214ab34c499e98287be550b2b331cde43ba69
Bishwasa <giribiswas415@gmail.com>
2024-11-24 21:30:48 +0530

    Done

--- my-project/src/components/Manager.jsx
+++ my-project/src/components/Manager.jsx
@@ -2,13 +2,31 @@ import React from 'react'
 
 const Manager = () => {
   return (
-    <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
-        Hello World
-        <div className="text-white flex flex-col p-4">
-<input type="text" />
-<input type="text" />
-<input type="text" />
+    <div class="static inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+      <div className=" bg-slate-50 mycontainer">
+        <h1 className='text-4xl text font-bold text-center'>
+          <span className='text-green-700'>  &lt;</span>
+          Pass
+          <span className='text-green-700'>OP /&gt;</span>
+        </h1>
+
+        <p className='text-green-900 text-center text-lg'>Your Own Password Manager</p>
+        <div className=" flex flex-col p-4 ml-10 mr-10 ">
+          <input placeholder='Website Name' className='rounded-full py-1 px-4 border border-green-500' type="text" />
+          <div className="flex mt-6">
+            <input placeholder='User Name' className='rounded-full  w-[80%] py-1 px-4 border border-green-500' type="text" />
+            <input placeholder='Password' className='rounded-full ml-10 w-[20%] py-1 px-4 border border-green-500' type="password" />
+
+          </div>
+          
+          <button className='bg-green-500 w-fit rounded-full flex justify-center p-2 text-lg items-center'> <lord-icon
+            src="https://cdn.lordicon.com/jgnvfzqg.json"
+            trigger="hover"
+            className="w-[250px] h-[250px]">
+          </lord-icon>Add Password</button>
         </div>
+      </div>
+
     </div>
   )
 }
--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -3,8 +3,14 @@ import React from 'react'
 const Navbar = () => {
     return (
         <div>
-            <nav className='bg-green-200 flex justify-between px-4 items-center p-5'>
-                <div className="logo font-bold">PassOP</div>
+            <nav className='bg-green-200 flex justify-around h-14 items-center p-5'>
+                <div className="logo font-bold text-2xl">
+                    <span className='text-green-700'>  &lt;</span>
+                   
+                    Pass
+                    <span className='text-green-700'>OP /&gt;</span>
+                    
+                    </div>
                 <ul className='flex gap-3'>
                     <li><a className='hover:font-bold transition-all' href="#">Home</a></li>
                     <li><a className='hover:font-bold transition-all' href="#">About</a></li>
